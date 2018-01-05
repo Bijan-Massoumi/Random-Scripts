c63ceee6b9ff68701f2c8c8c9bb30049b1db710d
Bijan-Massoumi <bmassoumi@gmail.com>
2018-01-05 15:31:13 -0800

    everything but graphing

--- crypto_stats/get_holdings.py
+++ crypto_stats/get_holdings.py
@@ -4,6 +4,7 @@ import json
 import sys
 import sqlite3
 import datetime
+from bashplotlib.scatterplot import plot_scatter
 
 def calculate_stake(coins,prices):
     #calc
@@ -57,6 +58,10 @@ def create_table_strings(coins):
          value REAL
         );""")
 
+    return_list.append( """CREATE TABLE IF NOT EXISTS all_coins (
+         coin TEXT PRIMARY KEY
+        );""")
+
     for coin in coins:
         return_list.append( """CREATE TABLE IF NOT EXISTS {} (
              id integer PRIMARY KEY,
@@ -79,13 +84,36 @@ def update_tables(conn,coins,cash):
             if coin == 'total':
                 continue
 
+            add_to_coin_list(c, coin)
+
             c.execute(''' INSERT INTO {}(date,num_coins,value)
               VALUES(?,?,?) '''.format(coin.lower()) , (now.strftime("%Y-%m-%d"), coins[coin], cash[coin]))
         conn.commit()
     except Error as e:
         print(e)
 
+def add_to_coin_list(cur, coin):
+    cur.execute("""SELECT * FROM all_coins WHERE coin = '{}'""".format(coin))
+    switch_val = cur.fetchall()
+    if(switch_val == []):
+        cur.execute(''' INSERT INTO all_coins(coin)
+                  VALUES('{}') '''.format(coin))
 
+def get_all_totals_by_day(conn):
+    try:
+        c =  conn.cursor()
+        c.execute("""SELECT date, avg(value) FROM total GROUP BY date;""")
+        return c.fetchall()
+    except Error as e:
+        print(e)
+
+def get_all_totals_for_day(conn, date): #2018-01-05 format
+    try:
+        c =  conn.cursor()
+        c.execute("""SELECT id, value FROM total WHERE date = '{}';""".format(date))
+        return c.fetchall()
+    except Error as e:
+        print(e)
 
 if __name__ == "__main__":
     db_file = "/Users/Bijibaba/Dropbox/Programs/random_scripts/crypto_stats/crypto_totals.db" #change to your own db
@@ -94,7 +122,7 @@ if __name__ == "__main__":
             coins = json.loads(input('Insert your coins labels and quantities  in "JSON Format", I.E. seperated by ":" and also seperating each pair by "," \n '
               'EX: {"ETH":3.4,"OMG":500}\n'))
         else:
-            coins = {'PIVX': 98, 'ETH':1.290, 'OMG':86.7, 'UFR': 1044}
+            coins = {'XZC': 11.87, 'ETH':1.033, 'OMG':86.7, 'UFR': 1044}
 
         prices = query_crypto_compare(coins)
         cash = calculate_stake(coins,prices)
@@ -107,6 +135,9 @@ if __name__ == "__main__":
         for table in tables:
             create_table(conn, table)
         update_tables(conn, coins, cash)
+        all_vals = get_all_totals_by_day(conn)
+        todays_vals = get_all_totals_for_day(conn,"2018-01-05")
+        #plot_scatter(todays_vals)
     else:
         print("Cannot create the database connection.\n Did you create your own "
               "sqllite db and change the db_file variable?")
